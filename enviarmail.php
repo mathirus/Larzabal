ff49919d065298daeb68dd7f036670a6cdc7187e
mathias <mathirusln@hotmail.com>
2018-11-20 22:29:15 -0300

    se modifico *botones de whatsapp/mail *agrego jquery *agrego funcionalidad al form

---
+++ enviarmail.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	
+<?php
+$destino= "hernan@tucontadorpublico.com.ar";
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$email = $_POST['email'];
+$consulta = $_POST['consulta'];
+$asunto = "Has recibido una nueva consulta";
+$contenido = "Nombre: " .$nombre ."<br>correo: " . $email . "<br>telefono: " . $telefono . "Ha escrito: " . $consulta;
+
+
+
+mail($destino,$asunto,$contenido);
+
+
+
+if(mail($destino,$asunto,$contenido)){
+	echo "Gracias por su consulta ".$nombre." nos estaremos comunicando a la brevedad";}else{
+		echo "Hubo un error";	}
+
+
+
+
+?>
+
+</body>
+</html>
